f6de364e39eb192b23c390225301e2b0822d8479
Blake Prejean <53191205+Mythidas@users.noreply.github.com>
2025-08-19 23:16:06 -0500

    Create Build.lua

---
+++ Build.lua
@@ -0,0 +1,30 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
+
+	files
+	{
+        "*.h",
+        "*.cpp"
+	}
+
+    includedirs
+    {
+        "../imgui/",
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
